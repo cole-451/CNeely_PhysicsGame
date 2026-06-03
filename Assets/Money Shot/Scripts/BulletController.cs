60474ad55c83f2766fabe4fcca5d9536c716fe6c
Cole <coleneely87@gmail.com>
2026-06-02 23:31:09 -0600

    tried to make a gliding system, got a basic boost; will cry later

--- Assets/Money Shot/Scripts/BulletController.cs
+++ Assets/Money Shot/Scripts/BulletController.cs
@@ -2,15 +2,49 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float baseSpeed;
+     private float currentThrustSpeed;
+    [SerializeField] private float maxThrustSpeed;
+    [SerializeField] private float minThrustSpeed;
+    [SerializeField] private float thrustFactor;
+
+    [SerializeField] private Transform cameraTransform;
+
+    private Rigidbody rb;
+
     void Start()
+    {
+        cameraTransform = Camera.main.transform.parent;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        GlidingMovement();
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GlidingMovement()
     {
-        
+        // pitch will use a sine wave, cause pointing up will make us go down, while pointing down will make us speed up
+        float pitchInRads = transform.eulerAngles.x * Mathf.Deg2Rad;
+        float mappedPitch = Mathf.Sin(pitchInRads) * thrustFactor;
+        Vector3 glidingForce = Vector3.forward * currentThrustSpeed;
+
+        currentThrustSpeed += mappedPitch;
+
+        currentThrustSpeed = Mathf.Clamp(currentThrustSpeed, 0, maxThrustSpeed);
+
+        rb.AddRelativeForce(Vector3.forward * baseSpeed);
+        //rb.AddRelativeForce(glidingForce);
+    }
+    private void ManagePlaneRotation()
+    {
+        Quaternion targetRotation = Quaternion.Euler(cameraTransform.eulerAngles.x, cameraTransform.eulerAngles.y, transform.eulerAngles.z);
+        transform.rotation = targetRotation;
     }
 }
